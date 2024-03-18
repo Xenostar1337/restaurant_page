54608a572d480084c44e07cbce097cf8caf3a6ad
Xenostar <157074608+Xenostar1337@users.noreply.github.com>
2024-03-18 00:43:47 -0600

    added some, broke some, fixed some, making progress

---
+++ src_home_js.main.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["src_home_js"],{
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ (() => {
+
+eval("const homeButton = document.querySelector(\"#home-btn\");\nconst contentHolder = document.getElementById(\"content\");\nconst newContent = document.createElement('p');\nhomeButton.style.backgroundColor = \"rgba(0, 15, 17.5, 0.9)\";\nnewContent.textContent = \"World Famous Grim Jim's Burgers: Juicy, flavorful patties grilled to perfection, nestled between fresh buns, delivering an unmatched taste sensation to burger enthusiasts worldwide.\";\ncontentHolder.appendChild(newContent);\n\n//append to card\n\n//# sourceURL=webpack://restaurant_page/./src/home.js?");
+
+/***/ })
+
+}]);
